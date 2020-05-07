bfb26bcd16ac9457cb91aed2a31f56c214c31cfc
JOSE CRUZ <joseacf1@gmail.com>
2020-05-07 12:40:14 -0300

    test.py

---
+++ test.py
@@ -0,0 +1,48 @@
+
+print (5>3 and 3<4) 
+
+# or
+print (5>3 and 55 < 10)
+
+
+exit ()
+
+print(5>3)
+print(3<10)
+print(5==5) # igualdade
+print(3!=2) # diferenca
+print(5 >= 10)
+print(6 <= 12)
+True 
+
+teste = 5 == 5
+
+exit ()
+num1 = 12
+num2 = 15
+print(num1+num2)
+
+num1 = int(input ('primeiro numero'))
+num2 = float(input ('segundo numero'))
+print (num1+num2)
+
+
+exit()
+
+num1= input ('primeiro numero')
+num2= input ('segundo numero')
+print (num1+num2) #junta informacao 
+print (num1*2)
+
+
+
+
+
+exit()
+
+#!/usr/bin/python3
+print ("Hello")
+nome = input ('digite nome:')
+print ('Ola, meu nome e:', nome)
+
+
